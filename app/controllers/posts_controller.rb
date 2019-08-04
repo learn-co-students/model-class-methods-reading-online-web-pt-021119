4578b1eb10b2059f6296d50d2656cb7c64022fec
github username <github email address>
2019-08-04 17:25:45 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,20 @@
 class PostsController < ApplicationController
+  
 
-  def index
-    @posts = Post.all
-  end
+  def index 
+    @authors = Author.all
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      @posts = Post.all
+    end 
+  end 
 
   def show
     @post = Post.find(params[:id])
@@ -13,18 +25,24 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params)
+    @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   def edit
     @post = Post.find(params[:id])
   end
+
+  private 
+  
+  def post_params
+    params.require(:posts).permit(:title, :description)
+  end 
 end
